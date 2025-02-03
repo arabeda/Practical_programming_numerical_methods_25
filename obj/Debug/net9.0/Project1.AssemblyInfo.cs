b1dd40acde1d3f2bce564aaaf1f09bb5ec121327
arabeda <rabeda.aleksandra@gmail.com>
2025-02-03 12:24:54 +0100

    Local changes

--- obj/Debug/net9.0/Project1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a19c1f1f54b82b12ca0621a2da12612deb921a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32082fa2a31d36181cac997cc0b518ceaf9f4e36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
